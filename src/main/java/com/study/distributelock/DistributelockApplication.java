b3fece8df84c52a74d767f1e86b71a07851dd231
luohx <Lhongxiao@lcchain.com>
2021-11-27 00:46:36 +0800

    各分布式锁实例

---
+++ src/main/java/com/study/distributelock/DistributelockApplication.java
@@ -0,0 +1,13 @@
+package com.study.distributelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributelockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributelockApplication.class, args);
+    }
+
+}
